b3efbfc40ea2a142d615e2ac66b5cc5323ab52fc
Borna Sirovec <fallens911@gmail.com>
2020-07-15 15:39:31 +0200

    day03

---
+++ day03/DB_creates_and_inputs.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE Owner(
+	owner_id INT CONSTRAINT owner_pk PRIMARY KEY,
+	firstName VARCHAR(20) NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	age INT NOT NULL,
+	town VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Vehicle(
+	vehicle_id INT CONSTRAINT vehicle_pk PRIMARY KEY,
+	model VARCHAR(20) NOT NULL,
+	kilometers INT NOT NULL,
+	color VARCHAR(20) NOT NULL,
+	owner_id INT CONSTRAINT vehicle_fk_owner REFERENCES Owner(owner_id)
+);
+
+INSERT INTO Owner values (1,'Borna','Sirovec',20,'Valpovo');
+INSERT INTO Owner values (2,'Rene','Šandor',21,'Belišće');
+INSERT INTO Owner values (3,'Arijan','Fuis',22,'Valpovo');
+INSERT INTO Owner values (4,'Luka','Štrok',20,'Valpovo');
+INSERT INTO Owner values (5,'Petar','Čvagić',20,'Šag');
+INSERT INTO Owner values (6,'Dario','Zavišić',21,'Vinkovci');
+INSERT INTO Owner values (7,'Luka','Žunić',19,'Zadar');
+
+INSERT INTO Vehicle values (1,'Astra',215000,'black',1);
+INSERT INTO Vehicle values (2,'A6',15000,'red',1);
+INSERT INTO Vehicle values (3,'A1',150000,'white',2);
+INSERT INTO Vehicle values (4,'Punto',214000,'purple',3);
+INSERT INTO Vehicle values (5,'Corolla',300000,'mild green',3);
+INSERT INTO Vehicle values (6,'R34',21000,'grey',4);
+INSERT INTO Vehicle values (7,'E36',299999,'blue',4);
+INSERT INTO Vehicle values (8,'Corsa',123000,'black',4);
+INSERT INTO Vehicle values (9,'C_TWO',524,'white',5);
+INSERT INTO Vehicle values (10,'Sharan',214710,'black',5);
+INSERT INTO Vehicle values (11,'Golf',245136,'pink',6);
+INSERT INTO Vehicle values (12,'Sccirocco',189620,'red',6);
+INSERT INTO Vehicle values (13,'Yugo 45',84210,'yellow',7);
+INSERT INTO Vehicle values (14,'RMax',745320,'gold',7);
+INSERT INTO Vehicle values (15,'Octavia',382423,'black',7);
+INSERT INTO Vehicle values (16,'Focus',123456,'black',7);
---
+++ day03/VS_DBController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.Data.Common;
+
+namespace Connecting_to_db.Controllers
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public Person(string fName, string lName)
+        {
+            firstName = fName;
+            lastName = lName;
+        }
+    }
+
+
+
+    public class VS_DBController : ApiController
+    {
+        public static SqlConnection _con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=day03DB;Integrated Security=True");
+   
+        
+
+
+
+        [Route("api/getnames")]
+        public HttpResponseMessage GetNames()
+        {
+            var people = new List<Person> {};
+
+            SqlConnection connection = _con;
+            using (connection) {
+                SqlCommand command = new SqlCommand("SELECT firstName, lastName FROM dbo.Owner;", connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetString(0), reader.GetString(1));
+                        people.Add(new Person(reader.GetString(0), reader.GetString(1)));
+                        
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, people);
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "empty");
+                }
+                reader.Close();
+
+            }
+           
+            }
+
+
+
+
+
+
+        [Route("api/VehByOwner")]
+        public HttpResponseMessage GetVeh([FromUri]int inptId)
+        {
+            var vozila = new List<string> { };
+            SqlConnection connection = _con;
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand("SELECT firstName, lastName, Owner.owner_id, model, color FROM dbo.Owner, dbo.Vehicle WHERE Owner.owner_id=Vehicle.owner_id " , connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                      
+                        Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader.GetString(0), reader.GetString(1), reader.GetInt32(2), reader.GetString(3), reader.GetString(4));
+                        if (reader.GetInt32(2) == inptId)
+                        {
+                            vozila.Add(reader.GetString(0));
+                            vozila.Add(reader.GetString(1));
+                            vozila.Add(reader.GetString(3));
+                            vozila.Add(reader.GetString(4));
+                            vozila.Add("\n");
+                        }
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, vozila);
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                    return Request.CreateResponse(HttpStatusCode.NotFound, inptId);
+                }
+                reader.Close();
+
+            }
+        }
+
+
+
+
+
+        [Route("api/insertOwner")]
+        public HttpResponseMessage Post([FromUri] int id, [FromUri] string firstName, [FromUri] string lastName, [FromUri] int age, [FromUri] string town)
+        {
+            SqlConnection connection = _con;
+            connection.Open();
+            using (var command = new SqlCommand("INSERT INTO OWNER VALUES(@id, @firstName, @lastName, @age, @town)", connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@firstName", firstName);
+                command.Parameters.AddWithValue("@lastName", lastName);
+                command.Parameters.AddWithValue("@age", age);
+                command.Parameters.AddWithValue("@town", town);
+                command.ExecuteNonQuery();
+                return Request.CreateResponse(HttpStatusCode.OK, "inserted");
+            }
+        }
+
+
+
+
+
+        [Route("api/update")]
+        public HttpResponseMessage Put([FromUri]int ownerID, [FromUri]int newAge)
+        {
+            SqlConnection connection = _con;
+            connection.Open();
+
+            using (var commmand = new SqlCommand("UPDATE Owner SET age = @newAge WHERE owner_id = @ownerID",connection))
+            {
+                commmand.Parameters.AddWithValue("@newAge", newAge);
+                commmand.Parameters.AddWithValue("@ownerID", ownerID);
+                commmand.ExecuteNonQuery();
+                return Request.CreateResponse(HttpStatusCode.OK, "Age updated ");
+            }
+        }
+
+
+
+
+
+        [Route("api/delete")]
+        public HttpResponseMessage Delete([FromUri]int inptId)
+        {
+            SqlConnection connection = _con;
+            connection.Open();
+            using (var command = new SqlCommand("DELETE FROM Owner WHERE owner_id = @inptId", connection))
+            {
+                command.Parameters.AddWithValue("@inptId", inptId);
+                command.ExecuteNonQuery();
+                return Request.CreateResponse(HttpStatusCode.OK, "deleted");
+            }
+
+        }
+    }
+}
